5bcefed634aad05d59d34d39c781c16ac22d9725
Viacheslav Muravev <muravjevslava@gmail.com>
2021-05-10 02:23:45 +0400

    CategoryTheoryTaskController updated with CustomColumnTableView

--- src/main/java/egl/client/controller/task/category/CategoryTheoryTaskController.java
+++ src/main/java/egl/client/controller/task/category/CategoryTheoryTaskController.java
@@ -1,50 +1,37 @@
 package egl.client.controller.task.category;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
-import egl.client.controller.ControllerUtils;
 import egl.client.controller.task.TheoryTaskController;
 import egl.client.model.topic.category.Category;
 import egl.client.model.topic.category.Translation;
-import egl.client.model.topic.category.Word;
+import egl.client.view.table.CustomColumnTableView;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.stereotype.Component;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 @Component
 @FxmlView
 public class CategoryTheoryTaskController extends TheoryTaskController {
 
-    @FXML private TableView<Translation> translationsTableView;
-    @FXML private TableColumn<Translation, Word> sourceTableColumn;
-    @FXML private TableColumn<Translation, Word> targetTableColumn;
+    @FXML private CustomColumnTableView<Translation> translationsTableView;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         super.initialize(url, resourceBundle);
-
-        sourceTableColumn.setCellValueFactory(new PropertyValueFactory<>("source"));
-        targetTableColumn.setCellValueFactory(new PropertyValueFactory<>("target"));
     }
 
     @Override
     public void rescaleViews(double parentWidth, double parentHeight) {
         descriptionTextArea.setPrefSize(parentWidth, parentHeight * 0.3);
         translationsTableView.setPrefSize(parentWidth, parentHeight * 0.6);
-        ControllerUtils.rescaleTableView(translationsTableView);
     }
 
     @Override
     protected void prepareSpecificTheory() {
         Category category = (Category) controllerTopic;
         var categoryTranslations = category.getTranslations();
-
-        var tableTranslations = translationsTableView.getItems();
-        tableTranslations.clear();
-        tableTranslations.addAll(categoryTranslations);
+        translationsTableView.showItems(categoryTranslations);
     }
 }
