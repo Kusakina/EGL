7963cc8e799d0b6ef7d2934c95223211df3e77a9
Viacheslav Muravev <muravjevslava@gmail.com>
2021-05-10 02:17:41 +0400

    Added ButtonColumn

---
+++ src/main/java/egl/client/view/table/ButtonColumn.java
@@ -0,0 +1,47 @@
+package egl.client.view.table;
+
+import javafx.beans.NamedArg;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.util.Callback;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.function.Consumer;
+
+@Getter
+@Setter
+public class ButtonColumn<T> extends TableColumn<T, Void> {
+
+    private Consumer<T> onAction;
+
+    public ButtonColumn(@NamedArg("text") String buttonText) {
+        Callback<TableColumn<T, Void>, TableCell<T, Void>> cellFactory = createCellFactory(buttonText);
+        setCellFactory(cellFactory);
+
+        this.onAction = (T) -> {};
+    }
+
+    private Callback<TableColumn<T, Void>, TableCell<T, Void>> createCellFactory(String buttonText) {
+        return (TableColumn<T, Void> param) -> new TableCell<>() {
+
+            final Button startButton = new Button(buttonText);
+
+            @Override
+            public void updateItem(Void fieldValue, boolean empty) {
+                super.updateItem(fieldValue, empty);
+                if (empty) {
+                    setGraphic(null);
+                } else {
+                    startButton.setOnAction(event -> {
+                        T item = getTableView().getItems().get(getIndex());
+                        onAction.accept(item);
+                    });
+                    setGraphic(startButton);
+                }
+                setText(null);
+            }
+        };
+    }
+}
