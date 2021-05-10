a78a830c91aed98f28054a72ff7b2ac3f4d4c50a
Viacheslav Muravev <muravjevslava@gmail.com>
2021-05-10 17:45:27 +0400

    LocalProfileService is @Service now

--- src/main/java/egl/client/service/model/profile/LocalProfileService.java
+++ src/main/java/egl/client/service/model/profile/LocalProfileService.java
@@ -3,7 +3,9 @@ package egl.client.service.model.profile;
 import egl.client.model.profile.LocalProfile;
 import egl.client.repository.profile.LocalProfileRepository;
 import egl.client.service.model.AbstractEntityService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class LocalProfileService extends AbstractEntityService<LocalProfile, LocalProfileRepository> {
 
     public LocalProfileService(LocalProfileRepository repository) {
