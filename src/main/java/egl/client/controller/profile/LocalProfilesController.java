6f77fb4484fd01bc14d94eaea5479898d3ea675c
Muravjev Slava <MuravjevSlava@gmail.com>
2021-05-10 23:59:32 +0300

    LocalProfilesController: Added onCreate and onEdit

--- src/main/java/egl/client/controller/profile/LocalProfilesController.java
+++ src/main/java/egl/client/controller/profile/LocalProfilesController.java
@@ -2,6 +2,7 @@ package egl.client.controller.profile;
 
 import egl.client.controller.Controller;
 import egl.client.model.profile.LocalProfile;
+import egl.client.service.FxmlService;
 import egl.client.service.model.profile.LocalProfileService;
 import egl.client.view.table.EntityServiceListView;
 import javafx.fxml.FXML;
@@ -16,6 +17,7 @@ import java.util.ResourceBundle;
 @RequiredArgsConstructor
 public class LocalProfilesController implements Controller {
 
+    private final FxmlService fxmlService;
     private final LocalProfileService localProfileService;
 
     @FXML private EntityServiceListView<LocalProfile> localProfilesListView;
@@ -25,10 +27,34 @@ public class LocalProfilesController implements Controller {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         localProfilesListView.setService(localProfileService);
         localProfilesListView.setOnSelect(this::onSelect);
+        localProfilesListView.setOnEdit(this::onEdit);
+
+        createProfileButton.setOnAction(event -> onCreate());
     }
 
     private void onSelect(LocalProfile localProfile) {
         localProfileService.select(localProfile);
+    }
+
+    private void onCreate() {
+        LocalProfile localProfile = new LocalProfile();
+        onEdit(localProfile, true);
+    }
+
+    private void onEdit(LocalProfile localProfile) {
+        onEdit(localProfile, false);
+    }
+
+    private void onEdit(LocalProfile localProfile, boolean isCreated) {
+        var changed = fxmlService.showInfoDialog(
+                LocalProfileInfoController.class,
+                localProfile,
+                "Новый пользователь", isCreated
+        );
+
+        if (changed) {
+            localProfileService.save(localProfile);
+        }
     }
 
     @Override
