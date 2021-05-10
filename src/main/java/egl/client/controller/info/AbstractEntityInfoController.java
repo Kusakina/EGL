09e9c04fd3b37df7c681a2b926d839bf8bceccbd
Viacheslav Muravev <muravjevslava@gmail.com>
2021-05-11 00:43:11 +0400

    AbstractEntityInfoController is now for all entities

--- src/main/java/egl/client/controller/info/AbstractEntityInfoController.java
+++ src/main/java/egl/client/controller/info/AbstractEntityInfoController.java
@@ -1,14 +1,12 @@
 package egl.client.controller.info;
 
-import egl.core.model.DescribedEntity;
+import egl.core.model.DatabaseEntity;
 import lombok.Getter;
-import lombok.Setter;
 
-public abstract class AbstractDescribedEntityInfoController<T extends DescribedEntity> implements EntityInfoController<T> {
+public abstract class AbstractEntityInfoController<T extends DatabaseEntity> implements EntityInfoController<T> {
 
     @Getter protected T entity;
     protected boolean isCreated;
-    @Setter protected String nameLabelText;
 
     @Override
     public void setContext(T entity, boolean isCreated) {
