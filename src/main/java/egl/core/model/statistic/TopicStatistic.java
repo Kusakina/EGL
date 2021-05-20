12f594a8f79e01060b3a043e436ff293a5efdb9b
Viacheslav Muravev <muravjevslava@gmail.com>
2021-05-21 00:39:44 +0400

    Added TopicStatistic

---
+++ src/main/java/egl/core/model/statistic/TopicStatistic.java
@@ -0,0 +1,49 @@
+package egl.core.model.statistic;
+
+import egl.core.model.DatabaseEntity;
+import egl.core.model.task.Task;
+import egl.core.model.topic.Topic;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.util.HashSet;
+import java.util.Set;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Data
+@NoArgsConstructor
+public class TopicStatistic extends DatabaseEntity {
+
+    @ManyToOne
+    private Topic topic;
+
+    @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.REFRESH, CascadeType.REMOVE})
+    private Set<TaskStatistic> taskStatistics;
+
+    public TopicStatistic(Topic topic) {
+        this.topic = topic;
+        this.taskStatistics = new HashSet<>();
+    }
+
+    public TaskStatistic getTaskStatisticFor(Task task) {
+        return taskStatistics.stream()
+                .filter(taskStatistic -> taskStatistic.getTask().equals(task))
+                .findAny().orElse(new TaskStatistic(task, Result.NONE));
+    }
+
+    public boolean updateBy(Task task, Result result) {
+        var taskStatistic = getTaskStatisticFor(task);
+
+        boolean better = taskStatistic.updateBy(result);
+        if (better) {
+            taskStatistics.add(taskStatistic);
+        }
+        return better;
+    }
+}
