e24a091ec881f70b1a2edcb1a1133011ef9a62b2
Viacheslav Muravev <muravjevslava@gmail.com>
2021-05-21 00:39:38 +0400

    Added TaskStatistic

---
+++ src/main/java/egl/core/model/statistic/TaskStatistic.java
@@ -0,0 +1,43 @@
+package egl.core.model.statistic;
+
+import egl.core.model.DatabaseEntity;
+import egl.core.model.task.Task;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import java.util.Optional;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Data
+@NoArgsConstructor
+public class TaskStatistic extends DatabaseEntity {
+
+    @ManyToOne
+    private Task task;
+
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REFRESH, CascadeType.REMOVE})
+    private Result result;
+
+    public TaskStatistic(Task task, Result result) {
+        this.task = task;
+        this.result = result;
+    }
+
+    public Result getResult() {
+        return Optional.ofNullable(result).orElse(Result.NONE);
+    }
+
+    public boolean updateBy(Result result) {
+        var better = getResult().compareTo(result) < 0;
+        if (better) {
+            this.result = result;
+        }
+        return better;
+    }
+}
