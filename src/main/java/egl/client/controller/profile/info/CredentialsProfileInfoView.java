ed7d1aa42b132e034b2ec9e899188382944de858
Muravjev Slava <MuravjevSlava@gmail.com>
2021-05-24 02:35:35 +0300

    Added CredentialsProfileInfoView

---
+++ src/main/java/egl/client/controller/profile/info/CredentialsProfileInfoView.java
@@ -0,0 +1,31 @@
+package egl.client.controller.profile.info;
+
+import egl.client.service.FxmlService;
+import egl.client.view.info.EntityInfoView;
+import egl.client.view.pane.CustomBorderPane;
+import egl.core.model.profile.Credentials;
+import javafx.fxml.FXML;
+
+public class CredentialsProfileInfoView extends CustomBorderPane implements EntityInfoView<Credentials> {
+
+    @FXML private ProfileInfoView profileInfoView;
+
+    public CredentialsProfileInfoView() {
+        FxmlService.loadView(this, CredentialsProfileInfoView.class, true);
+    }
+
+    @Override
+    public void initData(Credentials credentials, boolean isCreated) {
+        profileInfoView.initData(credentials.getProfile(), isCreated);
+    }
+
+    @Override
+    public void validateData() {
+        profileInfoView.validateData();
+    }
+
+    @Override
+    public void fillData(Credentials credentials) {
+        profileInfoView.fillData(credentials.getProfile());
+    }
+}
