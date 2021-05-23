b7ad56bbee59bd8975c28e9956ba07125c110c46
Muravjev Slava <MuravjevSlava@gmail.com>
2021-05-24 02:04:48 +0300

    Added GlobalProfileRepository and GlobalProfileService

---
+++ src/main/java/egl/client/repository/profile/GlobalProfileRepository.java
@@ -0,0 +1,8 @@
+package egl.client.repository.profile;
+
+import egl.client.model.profile.GlobalProfile;
+import egl.client.repository.EntityRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GlobalProfileRepository extends EntityRepository<GlobalProfile> { }
