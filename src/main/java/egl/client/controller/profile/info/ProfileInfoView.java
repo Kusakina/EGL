adb4c318200e27230e711e5090f4f2b0ec688010
Muravjev Slava <MuravjevSlava@gmail.com>
2021-05-24 02:34:03 +0300

    Added ProfileInfoView

---
+++ src/main/java/egl/client/controller/profile/info/ProfileInfoView.java
@@ -0,0 +1,32 @@
+package egl.client.controller.profile.info;
+
+import egl.client.service.FxmlService;
+import egl.client.view.info.EntityInfoView;
+import egl.client.view.info.NameDescriptionInfoView;
+import egl.client.view.pane.CustomBorderPane;
+import egl.core.model.profile.Profile;
+import javafx.fxml.FXML;
+
+public class ProfileInfoView extends CustomBorderPane implements EntityInfoView<Profile> {
+
+    @FXML private NameDescriptionInfoView<Profile> nameDescriptionInfoView;
+
+    public ProfileInfoView() {
+        FxmlService.loadView(this, ProfileInfoView.class, true);
+    }
+
+    @Override
+    public void initData(Profile profile, boolean isCreated) {
+        nameDescriptionInfoView.initData(profile, isCreated);
+    }
+
+    @Override
+    public void validateData() {
+        nameDescriptionInfoView.validateData();
+    }
+
+    @Override
+    public void fillData(Profile profile) {
+        nameDescriptionInfoView.fillData(profile);
+    }
+}
