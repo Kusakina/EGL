5bfc0584323fd1b6447c018009c2123e06ac2192
Muravjev Slava <MuravjevSlava@gmail.com>
2021-05-24 01:46:07 +0300

    Added global profile and credentials entity

---
+++ src/main/java/egl/global/model/profile/GlobalCredentials.java
@@ -0,0 +1,12 @@
+package egl.global.model.profile;
+
+import javax.persistence.Entity;
+
+import egl.core.model.profile.Credentials;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+public class GlobalCredentials extends Credentials {
+
+}
