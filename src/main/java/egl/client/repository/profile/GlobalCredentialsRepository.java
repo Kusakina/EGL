26fb9caa040f61922155b5dbbcef43503d14f2b7
Muravjev Slava <MuravjevSlava@gmail.com>
2021-05-24 05:27:55 +0300

    GlobalCredentialsRepository: added 'findByProfile' method

--- src/main/java/egl/client/repository/profile/GlobalCredentialsRepository.java
+++ src/main/java/egl/client/repository/profile/GlobalCredentialsRepository.java
@@ -2,7 +2,11 @@ package egl.client.repository.profile;
 
 import egl.client.model.profile.GlobalCredentials;
 import egl.client.repository.EntityRepository;
+import egl.core.model.profile.Profile;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GlobalCredentialsRepository extends EntityRepository<GlobalCredentials> { }
+public interface GlobalCredentialsRepository extends EntityRepository<GlobalCredentials> {
+
+    GlobalCredentials findByProfile(Profile profile);
+}
--- src/main/java/egl/client/service/model/profile/GlobalCredentialsService.java
+++ src/main/java/egl/client/service/model/profile/GlobalCredentialsService.java
@@ -3,6 +3,7 @@ package egl.client.service.model.profile;
 import egl.client.model.profile.GlobalCredentials;
 import egl.client.repository.profile.GlobalCredentialsRepository;
 import egl.client.service.model.AbstractEntityService;
+import egl.core.model.profile.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,5 +11,9 @@ public class GlobalCredentialsService extends AbstractEntityService<GlobalCreden
 
     public GlobalCredentialsService(GlobalCredentialsRepository repository) {
         super(repository);
+    }
+
+    public GlobalCredentials findBy(Profile profile) {
+        return repository.findByProfile(profile);
     }
 }
