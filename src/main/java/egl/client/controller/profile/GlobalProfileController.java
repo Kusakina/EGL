ec268184b487ad024d152bc1bfb6854f876a27fe
Viacheslav Muravev <muravjevslava@gmail.com>
2021-05-25 09:08:35 +0400

    GlobalProfileController: added login views

--- src/main/java/egl/client/controller/profile/GlobalProfileController.java
+++ src/main/java/egl/client/controller/profile/GlobalProfileController.java
@@ -5,12 +5,30 @@ import egl.client.model.profile.GlobalProfile;
 import egl.client.service.FxmlService;
 import egl.client.service.model.profile.GlobalCredentialsService;
 import egl.client.service.model.profile.GlobalProfileService;
+import egl.client.view.text.LabeledTextField;
+import egl.core.model.profile.Credentials;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 import org.springframework.stereotype.Component;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 @Component
 public class GlobalProfileController extends ProfileSelectController<GlobalProfile> {
 
     private final GlobalCredentialsService globalCredentialsService;
+
+    @FXML private GridPane loginGridPane;
+    @FXML private Text loginInfoText;
+    @FXML private LabeledTextField loginTextField;
+    @FXML private LabeledTextField passwordTextField;
+    @FXML private Button editProfileButton;
+    @FXML private Button loginButton;
+    @FXML private Text errorText;
 
     public GlobalProfileController(
             FxmlService fxmlService,
@@ -52,11 +70,60 @@ public class GlobalProfileController extends ProfileSelectController<GlobalProfi
 
     @Override
     public void setPrefSize(double parentWidth, double parentHeight) {
+        loginGridPane.setPrefSize(parentWidth, parentHeight);
+    }
 
+    private void showSelectedProfile() {
+        var selectedProfile = profileService.getSelectedProfile();
+
+        boolean selected = (null != selectedProfile);
+        String profileText = (selected ? selectedProfile.getName() : "не выбран");
+        loginInfoText.setText("Глобальный профиль: " + profileText);
+
+        editProfileButton.setVisible(selected);
     }
 
     @Override
     public void prepareToShow() {
+        showSelectedProfile();
+    }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        super.initialize(url, resourceBundle);
+
+        editProfileButton.setOnAction(event -> onEdit(profileService.getSelectedProfile()));
+
+        initLogin();
+    }
+
+    private void initLogin() {
+        loginButton.setOnAction(event -> onLogin());
+    }
+
+    private void showLoginError() {
+        errorText.setText("Некорректный логин/пароль");
+    }
+
+    private void onLogin() {
+        String login = loginTextField.getText();
+        String password = passwordTextField.getText();
+
+        Credentials credentials = globalCredentialsService.findBy(login);
+        if (null == credentials) {
+            showLoginError();
+            return;
+        }
+
+        long loginPasswordHash = Credentials.calculatePasswordHash(password);
+        if (credentials.getPasswordHash() != loginPasswordHash) {
+            showLoginError();
+            return;
+        }
+
+        errorText.setText("");
+
+        profileService.select((GlobalProfile) credentials.getProfile());
+        showSelectedProfile();
     }
 }
