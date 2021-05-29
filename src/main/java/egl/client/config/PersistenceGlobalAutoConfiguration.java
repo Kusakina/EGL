4bd714dba087cdf09fe5614aabbd03491c6e5081
Viacheslav Muravev <muravjevslava@gmail.com>
2021-05-29 18:16:25 +0400

    PersistenceGlobalAutoConfiguration extends PersistenceAutoConfiguration

--- src/main/java/egl/client/config/PersistenceGlobalAutoConfiguration.java
+++ src/main/java/egl/client/config/PersistenceGlobalAutoConfiguration.java
@@ -1,61 +1,41 @@
 package egl.client.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
-import java.util.HashMap;
 
 @Configuration
 @EnableJpaRepositories(
         basePackages = { "egl.client.repository.global" },
         entityManagerFactoryRef = "globalEntityManager",
         transactionManagerRef = "globalTransactionManager")
-@RequiredArgsConstructor
-public class PersistenceGlobalAutoConfiguration {
+public class PersistenceGlobalAutoConfiguration extends PersistenceAutoConfiguration {
 
-    private final Environment env;
+    public PersistenceGlobalAutoConfiguration(Environment env) {
+        super(env, "global");
+    }
 
     @Bean
-    @Primary
     @ConfigurationProperties(prefix="spring.global-datasource")
     public DataSource globalDataSource() {
-        return DataSourceBuilder.create().build();
+        return dataSource();
     }
 
-    @Primary
     @Bean
     public LocalContainerEntityManagerFactoryBean globalEntityManager() {
-        final LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-        em.setDataSource(globalDataSource());
-        em.setPackagesToScan("egl.client.model.global", "egl.client.model.core");
-
-        final HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        em.setJpaVendorAdapter(vendorAdapter);
-        final HashMap<String, Object> properties = new HashMap<>();
-        properties.put("spring.jpa.hibernate.ddl-auto", env.getProperty("spring.jpa.hibernate.ddl-auto"));
-        properties.put("spring.jpa.database-platform", env.getProperty("spring.jpa.database-platform"));
-        em.setJpaPropertyMap(properties);
-
-        return em;
+        return entityManager();
     }
 
-    @Primary
     @Bean
     public PlatformTransactionManager globalTransactionManager() {
-        final JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(globalEntityManager().getObject());
-        return transactionManager;
+        return transactionManager();
     }
+
 
 }
