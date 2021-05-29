baeeedfe2114d1a8030f97d524cea2ab3fb3cbe7
Viacheslav Muravev <muravjevslava@gmail.com>
2021-05-29 20:56:10 +0400

    Added LocalTestRepository

---
+++ src/main/java/egl/client/repository/local/task/LocalTestRepository.java
@@ -0,0 +1,12 @@
+package egl.client.repository.local.task;
+
+import egl.client.model.core.task.Task;
+import egl.client.model.core.task.Test;
+import egl.client.repository.EntityRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LocalTestRepository extends EntityRepository<Test> {
+
+    Test findByTask(Task task);
+}
