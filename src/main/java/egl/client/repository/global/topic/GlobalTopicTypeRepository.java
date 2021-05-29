7479d861b14366b24d75491d10aaa686c9fbf25e
Viacheslav Muravev <muravjevslava@gmail.com>
2021-05-29 20:54:09 +0400

    Topic type repositories: specified transactional and entity managers

--- src/main/java/egl/client/repository/global/topic/GlobalTopicTypeRepository.java
+++ src/main/java/egl/client/repository/global/topic/GlobalTopicTypeRepository.java
@@ -3,6 +3,11 @@ package egl.client.repository.global.topic;
 import egl.client.model.core.topic.TopicType;
 import egl.client.repository.EntityRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.PersistenceContext;
 
 @Repository
+@Transactional("globalTransactionManager")
+@PersistenceContext(name = "globalEntityManager")
 public interface GlobalTopicTypeRepository extends EntityRepository<TopicType> { }
--- src/main/java/egl/client/repository/local/topic/LocalTopicTypeRepository.java
+++ src/main/java/egl/client/repository/local/topic/LocalTopicTypeRepository.java
@@ -3,6 +3,11 @@ package egl.client.repository.local.topic;
 import egl.client.model.core.topic.TopicType;
 import egl.client.repository.EntityRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.PersistenceContext;
 
 @Repository
+@Transactional("localTransactionManager")
+@PersistenceContext(name = "localEntityManager")
 public interface LocalTopicTypeRepository extends EntityRepository<TopicType> { }
