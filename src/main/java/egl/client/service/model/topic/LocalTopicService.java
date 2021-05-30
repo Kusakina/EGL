8fcf935e84181bdc6ff04aa7d7c31d4738992d20
Viacheslav Muravev <muravjevslava@gmail.com>
2021-05-30 17:21:07 +0400

    LocalTopicService marked as Service

--- src/main/java/egl/client/service/model/topic/LocalTopicService.java
+++ src/main/java/egl/client/service/model/topic/LocalTopicService.java
@@ -4,10 +4,12 @@ import egl.client.model.core.topic.Topic;
 import egl.client.model.core.topic.TopicType;
 import egl.client.repository.local.topic.LocalTopicRepository;
 import egl.client.service.model.AbstractEntityService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public abstract class LocalTopicService extends AbstractEntityService<Topic, LocalTopicRepository> {
+@Service
+public class LocalTopicService extends AbstractEntityService<Topic, LocalTopicRepository> {
 
     public LocalTopicService(LocalTopicRepository repository) {
         super(repository);
