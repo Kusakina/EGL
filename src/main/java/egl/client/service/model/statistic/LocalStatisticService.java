5d58993834e2ad330cb56500a9dfaea12f5905f1
Muravjev Slava <MuravjevSlava@gmail.com>
2021-06-06 21:10:29 +0300

    All common actions moved from LocalStatisticService to StatisticService

--- src/main/java/egl/client/service/model/statistic/LocalStatisticService.java
+++ src/main/java/egl/client/service/model/statistic/LocalStatisticService.java
@@ -1,60 +1,17 @@
 package egl.client.service.model.statistic;
 
-import java.util.Optional;
-
 import egl.client.repository.local.statistic.LocalProfileStatisticRepository;
-import egl.client.model.core.profile.Profile;
-import egl.client.model.core.statistic.ProfileStatistic;
-import egl.client.model.core.statistic.TopicStatistic;
-import egl.client.model.core.topic.Topic;
 import egl.client.service.model.profile.LocalProfileService;
-import javafx.beans.property.Property;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 @Service
-@RequiredArgsConstructor
-public class LocalStatisticService {
+public class LocalStatisticService extends StatisticService {
 
-    private final LocalProfileService localProfileService;
-    private final LocalProfileStatisticRepository localProfileStatisticRepository;
-
-    private ProfileStatistic findBy(Profile profile) {
-        var profileStatistic = localProfileStatisticRepository.findByProfile(profile);
-        if (null == profileStatistic) {
-            profileStatistic = new ProfileStatistic(profile);
-            save(profileStatistic);
-        }
-
-        return profileStatistic;
-    }
-
-    public Optional<ProfileStatistic> getSelectedProfileStatistic() {
-        var profile = Optional.ofNullable(localProfileService.getSelectedProfile());
-        return profile.map(this::findBy);
-    }
-
-    public void save(ProfileStatistic profileStatistic) {
-        localProfileStatisticRepository.save(profileStatistic);
-    }
-
-    public Optional<TopicStatistic> findBy(Topic topic) {
-        return getSelectedProfileStatistic().map(profileStatistic ->
-            profileStatistic.getTopicStatisticFor(topic)
-            .orElseGet(() -> addStatisticFor(profileStatistic, topic))
-        );
-    }
-
-    private TopicStatistic addStatisticFor(ProfileStatistic profileStatistic, Topic topic) {
-        var topicStatistic = new TopicStatistic(profileStatistic, topic);
-        profileStatistic.getTopicStatistics().add(topicStatistic);
-        save(profileStatistic);
-        return profileStatistic.getTopicStatisticFor(topic).orElseThrow();
-    }
-
-    public void save(TopicStatistic topicStatistic) {
-        save(topicStatistic.getProfileStatistic());
+    public LocalStatisticService(
+            LocalProfileService profileService,
+            LocalProfileStatisticRepository profileStatisticRepository) {
+        super(profileService, profileStatisticRepository);
     }
 }
---
+++ src/main/java/egl/client/service/model/statistic/StatisticService.java
@@ -0,0 +1,55 @@
+package egl.client.service.model.statistic;
+
+import java.util.Optional;
+
+import egl.client.model.core.profile.Profile;
+import egl.client.model.core.statistic.ProfileStatistic;
+import egl.client.model.core.statistic.TopicStatistic;
+import egl.client.model.core.topic.Topic;
+import egl.client.repository.core.statistic.ProfileStatisticRepository;
+import egl.client.service.model.profile.ProfileService;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public abstract class StatisticService {
+
+    protected final ProfileService profileService;
+    protected final ProfileStatisticRepository profileStatisticRepository;
+
+    private ProfileStatistic findBy(Profile profile) {
+        var profileStatistic = profileStatisticRepository.findByProfile(profile);
+        if (null == profileStatistic) {
+            profileStatistic = new ProfileStatistic(profile);
+            save(profileStatistic);
+        }
+
+        return profileStatistic;
+    }
+
+    public Optional<ProfileStatistic> getSelectedProfileStatistic() {
+        var profile = Optional.ofNullable(profileService.getSelectedProfile());
+        return profile.map(this::findBy);
+    }
+
+    public void save(ProfileStatistic profileStatistic) {
+        profileStatisticRepository.save(profileStatistic);
+    }
+
+    public Optional<TopicStatistic> findBy(Topic topic) {
+        return getSelectedProfileStatistic().map(profileStatistic ->
+            profileStatistic.getTopicStatisticFor(topic)
+            .orElseGet(() -> addStatisticFor(profileStatistic, topic))
+        );
+    }
+
+    private TopicStatistic addStatisticFor(ProfileStatistic profileStatistic, Topic topic) {
+        var topicStatistic = new TopicStatistic(profileStatistic, topic);
+        profileStatistic.getTopicStatistics().add(topicStatistic);
+        save(profileStatistic);
+        return profileStatistic.getTopicStatisticFor(topic).orElseThrow();
+    }
+
+    public void save(TopicStatistic topicStatistic) {
+        save(topicStatistic.getProfileStatistic());
+    }
+}
