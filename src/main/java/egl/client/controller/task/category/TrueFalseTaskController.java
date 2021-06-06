703bfd225353adb1654ebf22ae46d8f71525073d
Muravjev Slava <MuravjevSlava@gmail.com>
2021-06-06 17:56:15 +0300

    TrueFalseTaskController.MAX_TASKS_COUNT -> application.properties

--- src/main/java/egl/client/controller/task/category/TrueFalseTaskController.java
+++ src/main/java/egl/client/controller/task/category/TrueFalseTaskController.java
@@ -8,6 +8,7 @@ import egl.client.service.model.topic.LocalTopicInfoService;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 import egl.client.controller.task.AbstractTaskController;
@@ -24,6 +25,7 @@ import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import net.rgielen.fxweaver.core.FxmlView;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -34,16 +36,22 @@ import java.util.List;
 @FxmlView
 public class TrueFalseTaskController extends LocalTaskController<Category> {
 
-    private static final int MAX_QUESTIONS_COUNT = 8;
+    @FXML
+    private VBox tasksVBox;
+
+    @Value("${task.true_false.max_tasks_count}")
+    private int maxTasksCount;
+
+    private final Random random;
+
     private List<TrueFalseQuestionView> questionViews;
     private List<Translation> translations;
     private List<Translation> answers;
 
-    @FXML
-    private VBox tasksVBox;
-
     public TrueFalseTaskController(LocalTopicInfoService localTopicInfoService, CategoryService categoryService) {
         super(localTopicInfoService, categoryService);
+
+        this.random = new Random();
     }
 
     @Override
@@ -56,10 +64,10 @@ public class TrueFalseTaskController extends LocalTaskController<Category> {
         Category category = specificLocalTopic;
         this.translations = category.getTranslations();
         this.answers = category.getTranslations();
-        Collections.shuffle(translations);
-        Collections.shuffle(answers);
+        Collections.shuffle(translations, random);
+        Collections.shuffle(answers, random);
 
-        final int tasksCount = Math.min(MAX_QUESTIONS_COUNT, translations.size());
+        final int tasksCount = Math.min(maxTasksCount, translations.size());
         this.questionViews = new ArrayList<>();
         for (int i = 0; i < tasksCount; ++i) {
             var questionView = createTestQuestion(i);
@@ -67,9 +75,10 @@ public class TrueFalseTaskController extends LocalTaskController<Category> {
             questionViews.add(questionView);
         }
     }
+
     private TrueFalseQuestionView createTestQuestion(int index) {
-        int bool = (int) (Math.random() * 2);
-        if (bool == 0) return new TrueFalseQuestionView(translations.get(index), translations.get(index), index);
+        var isCorrect = random.nextBoolean();
+        if (isCorrect) return new TrueFalseQuestionView(translations.get(index), translations.get(index), index);
         else return new TrueFalseQuestionView(translations.get(index), answers.get(index), index);
     }
 
