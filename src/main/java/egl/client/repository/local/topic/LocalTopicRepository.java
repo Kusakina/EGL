a7342dd405a9784fd1f1410c5a51ee0efb363d79
Muravjev Slava <MuravjevSlava@gmail.com>
2021-06-07 03:44:41 +0300

    Added TopicRepository and GlobalTopicRepository

--- src/main/java/egl/client/repository/local/topic/LocalTopicRepository.java
+++ src/main/java/egl/client/repository/local/topic/LocalTopicRepository.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 import egl.client.model.core.topic.Topic;
 import egl.client.model.core.topic.TopicType;
-import egl.client.repository.core.EntityRepository;
+import egl.client.repository.core.task.TopicRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LocalTopicRepository extends EntityRepository<Topic> {
+public interface LocalTopicRepository extends TopicRepository {
 
     List<Topic> findByTopicType(TopicType topicType);
 }
