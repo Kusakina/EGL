cd9121f39950d4bd923c0da96447d388e86991bf
Muravjev Slava <MuravjevSlava@gmail.com>
2021-06-07 20:08:49 +0300

    GlobalTopicInfoService: added 'save(topic, hashCode, profile)' method

--- src/main/java/egl/client/service/model/topic/GlobalTopicInfoService.java
+++ src/main/java/egl/client/service/model/topic/GlobalTopicInfoService.java
@@ -1,5 +1,6 @@
 package egl.client.service.model.topic;
 
+import egl.client.model.core.profile.Profile;
 import egl.client.model.core.topic.Topic;
 import egl.client.model.global.topic.GlobalTopicInfo;
 import egl.client.repository.global.topic.GlobalTopicInfoRepository;
@@ -15,5 +16,13 @@ public class GlobalTopicInfoService extends AbstractEntityService<GlobalTopicInf
 
     public GlobalTopicInfo findBy(Topic topic, int localHashCode) {
         return repository.findByTopicAndLocalHashCode(topic, localHashCode);
+    }
+
+    public GlobalTopicInfo save(Topic localTopic, int localHashCode, Profile author) {
+        var globalTopicInfo = new GlobalTopicInfo(
+                localTopic, localHashCode, author
+        );
+
+        return save(globalTopicInfo);
     }
 }
