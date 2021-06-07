3948e3af4ffe70a318d1629454eb49ffed7541af
Muravjev Slava <MuravjevSlava@gmail.com>
2021-06-07 19:28:01 +0300

    Added GlobalTopicInfoRepository and GlobalTopicInfoService

---
+++ src/main/java/egl/client/service/model/topic/GlobalTopicInfoService.java
@@ -0,0 +1,19 @@
+package egl.client.service.model.topic;
+
+import egl.client.model.core.topic.Topic;
+import egl.client.model.global.topic.GlobalTopicInfo;
+import egl.client.repository.global.topic.GlobalTopicInfoRepository;
+import egl.client.service.model.AbstractEntityService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GlobalTopicInfoService extends AbstractEntityService<GlobalTopicInfo, GlobalTopicInfoRepository> {
+
+    public GlobalTopicInfoService(GlobalTopicInfoRepository repository) {
+        super(repository);
+    }
+
+    public GlobalTopicInfo findBy(Topic topic, int localHashCode) {
+        return repository.findByTopicAndLocalHashCode(topic, localHashCode);
+    }
+}
