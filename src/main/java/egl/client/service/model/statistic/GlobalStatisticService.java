19d2cd3cf03bd0a85c3e6f772fdeae94eab31af7
Muravjev Slava <MuravjevSlava@gmail.com>
2021-06-07 20:09:07 +0300

    GlobalStatisticService: added 'register(topic, hashCode)' method

--- src/main/java/egl/client/service/model/statistic/GlobalStatisticService.java
+++ src/main/java/egl/client/service/model/statistic/GlobalStatisticService.java
@@ -4,8 +4,10 @@ import java.util.Optional;
 
 import egl.client.model.core.statistic.TopicStatistic;
 import egl.client.model.core.topic.Topic;
+import egl.client.model.local.topic.LocalTopicInfo;
 import egl.client.repository.global.statistic.GlobalProfileStatisticRepository;
 import egl.client.service.model.profile.GlobalProfileService;
+import egl.client.service.model.topic.GlobalTopicInfoService;
 import egl.client.service.model.topic.GlobalTopicService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,18 +17,33 @@ import org.springframework.transaction.annotation.Transactional;
 public class GlobalStatisticService extends StatisticService {
 
     private final GlobalTopicService globalTopicService;
+    private final GlobalTopicInfoService globalTopicInfoService;
 
-    public GlobalStatisticService(
-            GlobalProfileService profileService,
-            GlobalProfileStatisticRepository profileStatisticRepository,
-            GlobalTopicService globalTopicService) {
+    public GlobalStatisticService(GlobalProfileService profileService,
+                                  GlobalProfileStatisticRepository profileStatisticRepository,
+                                  GlobalTopicService globalTopicService,
+                                  GlobalTopicInfoService globalTopicInfoService) {
         super(profileService, profileStatisticRepository);
         this.globalTopicService = globalTopicService;
+        this.globalTopicInfoService = globalTopicInfoService;
     }
 
     @Override
     public Optional<TopicStatistic> findBy(Topic localTopic) {
         return globalTopicService.findByLocal(localTopic)
                 .flatMap(super::findBy);
+    }
+
+    public long registerTopic(Topic topic, int localHashCode) {
+        var author = profileService.getSelectedProfile();
+        if (null == author) {
+            return LocalTopicInfo.NO_GLOBAL_ID;
+        }
+
+        var globalTopicInfo = globalTopicInfoService.save(
+                topic, localHashCode, author
+        );
+
+        return globalTopicInfo.getTopic().getId();
     }
 }
