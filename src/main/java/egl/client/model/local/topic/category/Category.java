b29e32cea0db4ee8e2a80f876357b76d829eade7
Muravjev Slava <MuravjevSlava@gmail.com>
2021-06-07 20:47:17 +0300

    Category uses Set now

--- src/main/java/egl/client/model/local/topic/category/Category.java
+++ src/main/java/egl/client/model/local/topic/category/Category.java
@@ -1,16 +1,24 @@
 package egl.client.model.local.topic.category;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
+
 import egl.client.model.core.DatabaseEntity;
 import egl.client.model.local.topic.LocalTopicInfo;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
-
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @EqualsAndHashCode(callSuper = true)
 @Entity
@@ -22,23 +30,30 @@ public class Category extends DatabaseEntity {
 
     @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH})
     @Fetch(value = FetchMode.SUBSELECT)
-    private List<Translation> translations;
+    private Set<Translation> translations;
+
+    private Category(LocalTopicInfo localTopicInfo) {
+        this.localTopicInfo = localTopicInfo;
+        this.translations = new LinkedHashSet<>();
+    }
 
     public Category() {
-        this.localTopicInfo = new LocalTopicInfo();
-        this.translations = new ArrayList<>();
+        this(new LocalTopicInfo());
     }
 
     public Category(Category other) {
         this.localTopicInfo = new LocalTopicInfo(other.getLocalTopicInfo());
         this.translations = other.getTranslations()
-                .stream().map(Translation::new).collect(Collectors.toList());
+                .stream().map(Translation::new).collect(Collectors.toSet());
     }
 
     public Category(LocalTopicInfo localTopicInfo,
-                    List<Translation> translations) {
-        this.localTopicInfo = localTopicInfo;
-        this.translations = translations;
+                    Collection<Translation> translations) {
+        this(localTopicInfo);
+        this.translations.addAll(translations);
+        this.localTopicInfo.setGlobalHashCode(
+                this.translations.hashCode()
+        );
     }
 
     public List<Translation> getTranslations() {
