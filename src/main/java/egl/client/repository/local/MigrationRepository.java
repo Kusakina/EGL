6e20ff7bd1855bc6b561c357170619040957a8a0
Muravjev Slava <MuravjevSlava@gmail.com>
2021-06-08 12:28:36 +0300

    Added MigrationInfo

---
+++ src/main/java/egl/client/repository/local/MigrationRepository.java
@@ -0,0 +1,7 @@
+package egl.client.repository.local;
+
+import egl.client.model.local.topic.MigrationInfo;
+import egl.client.repository.core.EntityRepository;
+
+public interface MigrationRepository extends EntityRepository<MigrationInfo> {
+}
