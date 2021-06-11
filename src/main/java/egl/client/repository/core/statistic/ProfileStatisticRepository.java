2d1692a534d73e9b623ecd3a04b2256b244dfdd3
Muravjev Slava <MuravjevSlava@gmail.com>
2021-06-11 22:11:51 +0300

    ProfileStatisticRepository.findByProfile returns Optional now

--- src/main/java/egl/client/repository/core/statistic/ProfileStatisticRepository.java
+++ src/main/java/egl/client/repository/core/statistic/ProfileStatisticRepository.java
@@ -1,10 +1,12 @@
 package egl.client.repository.core.statistic;
 
+import java.util.Optional;
+
 import egl.client.model.core.profile.Profile;
 import egl.client.model.core.statistic.ProfileStatistic;
 import egl.client.repository.core.EntityRepository;
 
 public interface ProfileStatisticRepository extends EntityRepository<ProfileStatistic> {
 
-    ProfileStatistic findByProfile(Profile profile);
+    Optional<ProfileStatistic> findByProfile(Profile profile);
 }
