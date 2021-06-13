6a1fc6a7c68237d3ae068e9267432b827145940c
Muravjev Slava <MuravjevSlava@gmail.com>
2021-06-13 22:15:04 +0300

    GlobalTaskStatisticRepository extends GlobalEntityManagerRepository

--- src/main/java/egl/client/repository/global/statistic/GlobalTaskStatisticRepository.java
+++ src/main/java/egl/client/repository/global/statistic/GlobalTaskStatisticRepository.java
@@ -1,9 +1,30 @@
 package egl.client.repository.global.statistic;
 
-import egl.client.model.core.statistic.TaskStatistic;
-import egl.client.repository.core.statistic.TaskStatisticRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Map;
+import java.util.Optional;
 
-public interface GlobalTaskStatisticRepository extends TaskStatisticRepository,
-        JpaRepository<TaskStatistic, Long> {
+import egl.client.model.core.statistic.TaskStatistic;
+import egl.client.model.core.statistic.TopicStatistic;
+import egl.client.repository.core.statistic.TaskStatisticRepository;
+import egl.client.repository.global.GlobalEntityManagerRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class GlobalTaskStatisticRepository
+        extends GlobalEntityManagerRepository<TaskStatistic>
+        implements TaskStatisticRepository {
+
+    @Override
+    protected Class<TaskStatistic> getEntityClass() {
+        return TaskStatistic.class;
+    }
+
+    @Override
+    public Optional<TaskStatistic> findByTopicStatisticAndTaskName(
+            TopicStatistic topicStatistic, String taskName) {
+        return findByFields(Map.of(
+                "topicStatistic", topicStatistic,
+                "taskName", taskName
+        ));
+    }
 }
