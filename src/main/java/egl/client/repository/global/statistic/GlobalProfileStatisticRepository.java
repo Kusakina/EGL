a7e2a35324cdcfba4fbc77bd08159cd266ee00e9
Muravjev Slava <MuravjevSlava@gmail.com>
2021-06-13 22:09:45 +0300

    GlobalProfileStatisticRepository extends GlobalEntityManagerRepository

--- src/main/java/egl/client/repository/global/statistic/GlobalProfileStatisticRepository.java
+++ src/main/java/egl/client/repository/global/statistic/GlobalProfileStatisticRepository.java
@@ -1,12 +1,25 @@
 package egl.client.repository.global.statistic;
 
+import java.util.Optional;
+
+import egl.client.model.core.profile.Profile;
 import egl.client.model.core.statistic.ProfileStatistic;
 import egl.client.repository.core.statistic.ProfileStatisticRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
+import egl.client.repository.global.GlobalEntityManagerRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GlobalProfileStatisticRepository extends ProfileStatisticRepository,
-        JpaRepository<ProfileStatistic, Long> {
+public class GlobalProfileStatisticRepository
+        extends GlobalEntityManagerRepository<ProfileStatistic>
+        implements ProfileStatisticRepository {
 
+    @Override
+    protected Class<ProfileStatistic> getEntityClass() {
+        return ProfileStatistic.class;
+    }
+
+    @Override
+    public Optional<ProfileStatistic> findByProfile(Profile profile) {
+        return findByField("profile", profile);
+    }
 }
