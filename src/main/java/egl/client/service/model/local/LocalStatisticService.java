72b552343ec1439d0a9624232deaf21512b161a4
Viacheslav Muravev <muravjevslava@gmail.com>
2021-06-16 11:14:58 +0400

    Removed TopicStatisticByLocalService

--- src/main/java/egl/client/service/model/local/LocalStatisticService.java
+++ src/main/java/egl/client/service/model/local/LocalStatisticService.java
@@ -1,19 +1,14 @@
 package egl.client.service.model.local;
 
-import java.util.Optional;
-
-import egl.client.model.core.statistic.TopicStatistic;
 import egl.client.model.core.topic.Topic;
 import egl.client.service.model.core.AbstractStatisticService;
-import egl.client.service.model.core.TopicStatisticByLocalService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 @Service
 public class LocalStatisticService
-        extends AbstractStatisticService
-        implements TopicStatisticByLocalService {
+        extends AbstractStatisticService {
 
     public LocalStatisticService(
             LocalProfileService profileService,
@@ -21,11 +16,6 @@ public class LocalStatisticService
             LocalTopicStatisticService topicStatisticService,
             LocalTaskStatisticService taskStatisticService) {
         super(profileService, profileStatisticService, topicStatisticService, taskStatisticService);
-    }
-
-    @Override
-    public Optional<TopicStatistic> findStatisticByLocal(Topic topic) {
-        return findBy(topic);
     }
 
     public void removeAllBy(Topic topic) {
